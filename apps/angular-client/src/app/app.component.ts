d1058baaf965e2bd15df64da53cbb3a38024eba8
Vladimir Drayling <draylegend@gmail.com>
2022-09-23 13:06:11 +0200

    feat(core): link "appointments" route with feature lib

--- apps/angular-client/src/app/app.component.ts
+++ apps/angular-client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'im-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [RouterOutlet],
 })
 export class AppComponent {}
--- libs/core/domain/src/lib/routes.ts
+++ libs/core/domain/src/lib/routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export default [] as Routes;
+export default [
+  {
+    path: 'appointments',
+    loadComponent: () =>
+      import('@immomio/appointment/feature').then(
+        m => m.AppointmentFeatureComponent,
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'appointments',
+  },
+] as Routes;
