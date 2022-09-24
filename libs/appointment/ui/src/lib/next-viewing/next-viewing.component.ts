b4d7000f15801b6e27818b862d241d37f7d801f0
Vladimir Drayling <draylegend@gmail.com>
2022-09-24 21:50:41 +0200

    feat(appointment-ui): add styles to next viewing component

--- libs/appointment/ui/src/lib/next-viewing/next-viewing.component.ts
+++ libs/appointment/ui/src/lib/next-viewing/next-viewing.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'im-next-viewing',
   standalone: true,
+  styleUrls: ['./next-viewing.component.scss'],
   templateUrl: './next-viewing.component.html',
+  imports: [MatIconModule],
 })
 export class NextViewingComponent {}
