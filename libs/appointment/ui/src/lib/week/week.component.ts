a96f349fd20a0748b7ba8ad90e11407a9d13f686
Vladimir Drayling <draylegend@gmail.com>
2022-09-24 22:39:32 +0200

    feat(appointment-ui): add header to week view

--- libs/appointment/ui/src/lib/week/week.component.ts
+++ libs/appointment/ui/src/lib/week/week.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  standalone: true,
   selector: 'im-week',
+  standalone: true,
+  styleUrls: ['./week.component.scss'],
   templateUrl: './week.component.html',
+  imports: [MatButtonModule, MatIconModule],
 })
 export class WeekComponent {}
