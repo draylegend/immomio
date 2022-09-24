c4bc7ebbb7787f0b045ed42b3cd90dd3ce4e706c
Vladimir Drayling <draylegend@gmail.com>
2022-09-24 10:33:42 +0200

    feat(appointment-feature): arrange ui components

--- libs/appointment/feature/src/lib/appointment-feature.component.ts
+++ libs/appointment/feature/src/lib/appointment-feature.component.ts
@@ -1,13 +1,25 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AppointmentFacade } from '@immomio/appointment/domain';
-import { FilterComponent } from '@immomio/appointment/ui';
+import {
+  CalendarComponent,
+  FilterComponent,
+  NextViewingComponent,
+  WeekComponent,
+} from '@immomio/appointment/ui';
 import { PushModule } from '@ngrx/component';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  styleUrls: ['./appointment-feature.component.scss'],
   templateUrl: './appointment-feature.component.html',
-  imports: [PushModule, FilterComponent],
+  imports: [
+    CalendarComponent,
+    FilterComponent,
+    NextViewingComponent,
+    PushModule,
+    WeekComponent,
+  ],
 })
 export class AppointmentFeatureComponent {
   constructor(public facade: AppointmentFacade) {}
--- libs/appointment/ui/src/index.ts
+++ libs/appointment/ui/src/index.ts
@@ -1,1 +1,4 @@
+export * from './lib/calendar/calendar.component';
 export * from './lib/filter/filter.component';
+export * from './lib/next-viewing/next-viewing.component';
+export * from './lib/week/week.component';
