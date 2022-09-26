0e5139deec0f6aef640a9c0fe9e242efdb19acc3
Vladimir Drayling <draylegend@gmail.com>
2022-09-27 00:04:24 +0200

    feat(appointment-details): position details page

--- libs/appointment/features/details-feature/src/lib/details-feature.component.ts
+++ libs/appointment/features/details-feature/src/lib/details-feature.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  styleUrls: ['./details-feature.component.scss'],
   templateUrl: './details-feature.component.html',
 })
 export class DetailsFeatureComponent {}
