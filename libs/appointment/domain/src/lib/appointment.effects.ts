3654b42d549c6d51434ccd246c0498e7feaa5df5
Vladimir Drayling <draylegend@gmail.com>
2022-09-27 12:26:14 +0200

    chore: remove log

--- libs/appointment/domain/src/lib/appointment.effects.ts
+++ libs/appointment/domain/src/lib/appointment.effects.ts
@@ -13,7 +13,6 @@ export class AppointmentEffects {
   load$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(appointmentActions.load),
-      tap(r => console.log(r)),
       switchMap(() => this.service.fetch()),
       map(res =>
         appointmentActions.loadsuccess({
